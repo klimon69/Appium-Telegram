59989ec6aa10d0850e978644152f321f594fd635
klimon69 <klim_fizik@bk.ru>
2019-05-06 19:41:06 +0300

    added .json file with Data

--- Tests/test.py
+++ Tests/test.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 from appium import webdriver
 from ScreenObjects.SendMessage import SendMsg
@@ -6,13 +7,15 @@ from ScreenObjects.SendMessage import SendMsg
 class AppTestAppium(unittest.TestCase):
 
   def setUp(self):
+      with open('../Data/data.json', encoding='utf-8') as data_file:
+          data = json.loads(data_file.read())
       desired_caps = {}
-      desired_caps['platformName'] = 'Android'
-      desired_caps['platformVersion'] = ''
-      desired_caps['deviceName'] = '81e9b20e'
-      desired_caps['appPackage'] = 'org.telegram.messenger'
-      desired_caps['appActivity'] = 'org.telegram.ui.LaunchActivity'
-      desired_caps['noReset'] = 'True'
+      desired_caps['platformName'] = data['platformName']
+      desired_caps['platformVersion'] = data['platformVersion']
+      desired_caps['deviceName'] = data['deviceName']
+      desired_caps['appPackage'] = data['appPackage']
+      desired_caps['appActivity'] = data['appActivity']
+      desired_caps['noReset'] = data['noReset']
       self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
 
   def tearDown(self):
